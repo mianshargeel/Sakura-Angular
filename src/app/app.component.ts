c63a6a6440d9e41aeaf4c21e1b797aba41d94582
mian shargeel <call2poma@gmail.com>
2025-03-14 18:33:25 +0100

    How to create variable in SCSS and @mixixn-function concept in SCSS explained in style.scsc and used in navbar-component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    MainContentComponent,
-    FooterComponent,
-    ImprintComponent
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
